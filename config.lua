ee27e9ff3b0c1e709280c0abf43257a03e51718c
Anho Ki <shiina@mayuri.kr>
2026-01-27 03:51:01 +0900

    Add the dummy settings interface

---
+++ config.lua
@@ -0,0 +1,225 @@
+local addonName, ns = ...
+
+-- Wow APIs
+local CreateFrame = CreateFrame -- luacheck: globals CreateFrame
+local MinimalSliderWithSteppersMixin = MinimalSliderWithSteppersMixin -- luacheck: globals MinimalSliderWithSteppersMixin, no max line length
+local Settings = Settings -- luacheck: globals Settings
+
+-- Libraries
+
+
+-- Local/session variables
+local util = ns.util
+
+-- `Euivin-Settings' is from SavedVariables
+-- luacheck: globals EuivinConfig
+
+local function EuivinInitConfig()
+  if EuivinConfig == nil then
+    EuivinConfig = {}
+  end
+  if EuivinConfig.UIScale == nil or next(EuivinConfig.UIScale) == nil then
+    EuivinConfig.UIScale = {
+      ["enable"] = false,
+      ["factor"] = 0.5,
+    }
+  end
+  if EuivinConfig.Stats == nil then
+    EuivinConfig.Stats = false
+  end
+  if EuivinConfig.Range == nil then
+    EuivinConfig.Range = false
+  end
+  if EuivinConfig.Mythic == nil or next(EuivinConfig.Mythic) == nil then
+    EuivinConfig.Mythic = {
+      ["enable"] = true,
+      ["goal"] = 10,
+    }
+  end
+  if EuivinConfig.Delve == nil then
+    EuivinConfig.Delve = true
+  end
+  if EuivinConfig.Crests == nil then
+    EuivinConfig.Crests = true
+  end
+  if EuivinConfig.Profession == nil then
+    EuivinConfig.Profession = true
+  end
+
+  local category, layout = Settings.RegisterVerticalLayoutCategory("Euivin Assistant")
+
+  local mythicCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_MYTHIC_ENABLED",
+    "enable",
+    EuivinConfig.Mythic,
+    Settings.VarType.Boolean,
+    "Enable Mythic+ tracker",
+    true
+  )
+  Settings.CreateCheckbox(
+    category,
+    mythicCheckboxSetting,
+    "Toggle whether to show the Mythic+ tracker."
+  )
+
+  local mythicSliderOption = Settings.CreateSliderOptions(2, 30, 1)
+  local mythicSliderSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_MYTHIC_GOAL",
+    "goal",
+    EuivinConfig.Mythic,
+    Settings.VarType.Number,
+    "Mythic+ Keystone Goal Level",
+    10
+  )
+  mythicSliderOption:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right)
+  Settings.CreateSlider(
+    category,
+    mythicSliderSetting,
+    mythicSliderOption,
+    "The goal level of the Mythic+ keystone for the Great Vault."
+  )
+
+  local delveCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_DELVE_ENABLED",
+    "Delve",
+    EuivinConfig,
+    Settings.VarType.Boolean,
+    "Enable Delves tracker",
+    true
+  )
+  Settings.CreateCheckbox(
+    category,
+    delveCheckboxSetting,
+    "Toggle whether to show the Delves/World activities tracker."
+  )
+
+  local crestsCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_CRESTS_ENABLED",
+    "Crests",
+    EuivinConfig,
+    Settings.VarType.Boolean,
+    "Enable Item Upgrade tracker",
+    true
+  )
+  Settings.CreateCheckbox(
+    category,
+    crestsCheckboxSetting,
+    "Toggle whether to show the currency tracker for the gear item level upgrade."
+  )
+
+  local professionCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_PROFESSION_ENABLED",
+    "Profession",
+    EuivinConfig,
+    Settings.VarType.Boolean,
+    "Enable Profession tracker",
+    true
+  )
+  Settings.CreateCheckbox(
+    category,
+    professionCheckboxSetting,
+    "Toggle whether to show the concentration and sparks tracker."
+  )
+
+  -- local nameplateTitleInitializer = ns.util.CreateSettingsListSectionHeaderInitializer("Nameplates")
+  -- layout:AddInitializer(nameplateTitleInitializer);
+
+  local extraTitleInitializer = ns.util.CreateSettingsListSectionHeaderInitializer("Extra modules")
+  layout:AddInitializer(extraTitleInitializer);
+
+  local uiScaleCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_UISCALE_ENABLED",
+    "enable",
+    EuivinConfig.UIScale,
+    Settings.VarType.Boolean,
+    "Enable UI Scale",
+    false
+  )
+  local uiScaleSliderOption = Settings.CreateSliderOptions(1, 64, 1)
+  local uiScaleSliderSetting = Settings.RegisterProxySetting(
+    category,
+    "EUIVIN_UISCALE_FACTOR",
+    Settings.VarType.Number,
+    "UI Scale factor",
+    50,
+    function()
+      return EuivinConfig.UIScale.factor * 100
+    end,
+    function(value)
+      EuivinConfig.UIScale.factor = value / 100
+    end
+  )
+  uiScaleSliderOption:SetLabelFormatter(
+    MinimalSliderWithSteppersMixin.Label.Right,
+    function(value)
+      return tostring(value) .. "%"
+    end
+  )
+  local uiScaleInitializer = util.CreateSettingsCheckboxSliderInitializer(
+    uiScaleCheckboxSetting,
+    "Enable UI Scale",
+    "Toggle whether to use UI Scale." ..
+    "\n\n|cffff0000" ..
+    "You must reload the UI after changing this." ..
+    "|r",
+    uiScaleSliderSetting,
+    uiScaleSliderOption,
+    "UI Scale factor",
+    "UI Scale factor in percentage."
+  )
+  layout:AddInitializer(uiScaleInitializer)
+
+  local statsCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_STATS_ENABLED",
+    "Stats",
+    EuivinConfig,
+    Settings.VarType.Boolean,
+    "Enable Stats graph",
+    false
+  )
+  Settings.CreateCheckbox(
+    category,
+    statsCheckboxSetting,
+    "Toggle whether to show the Stats graph." ..
+    "\n\n|cffff0000" ..
+    "You must reload the UI after changing this." ..
+    "|r"
+  )
+
+  local rangeCheckboxSetting = Settings.RegisterAddOnSetting(
+    category,
+    "EUIVIN_RANGE_ENABLED",
+    "Range",
+    EuivinConfig,
+    Settings.VarType.Boolean,
+    "Enable Range display",
+    false
+  )
+  Settings.CreateCheckbox(
+    category,
+    rangeCheckboxSetting,
+    "Toggle whether to show Ranges to the Target unit and the Focus unit." ..
+    "\n\n|cffff0000" ..
+    "You must reload the UI after changing this." ..
+    "|r"
+  )
+
+  Settings.RegisterAddOnCategory(category)
+end
+
+local hiddenFrame = CreateFrame("Frame")
+hiddenFrame:RegisterEvent("ADDON_LOADED")
+hiddenFrame:SetScript(
+  "OnEvent",
+  function(_, _, loadedAddon)
+    if loadedAddon == addonName then
+      EuivinInitConfig()
+    end
+end)
--- util.lua
+++ util.lua
@@ -2,6 +2,7 @@ local _, ns = ...
 
 -- Wow APIs
 local CreateFrame = CreateFrame -- luacheck: globals CreateFrame
+local Settings = Settings -- luacheck: globals Settings
 local UIParent = UIParent -- luacheck: globals UIParent
 local strsplittable = strsplittable -- luacheck: globals strsplittable
 
@@ -13,159 +14,188 @@ local LibSharedMedia = LibStub("LibSharedMedia-3.0")
 local data = ns.data
 
 if ns.util == nil then
-    ns.util = {}
+  ns.util = {}
 end
 
 -- Borrowed from WeakAuras
 -- WeakAuras is licensed under GPLv2
 ns.util.WA_Utf8Sub = function(input, size)
-    local output = ""
-    input = tostring(input)
-    if type(input) ~= "string" then
-        return output
-    end
-    local i = 1
-    while (size > 0) do
-        local byte = input:byte(i)
-        if not byte then
-            return output
-        end
-        if byte < 128 then
-            -- ASCII byte
-            output = output .. input:sub(i, i)
-            size = size - 1
-        elseif byte < 192 then
-            -- Continuation bytes
-            output = output .. input:sub(i, i)
-        elseif byte < 244 then
-            -- Start bytes
-            output = output .. input:sub(i, i)
-            size = size - 1
-        end
-        i = i + 1
-    end
-
-    -- Add any bytes that are part of the sequence
-    while (true) do
-        local byte = input:byte(i)
-        if byte and byte >= 128 and byte < 192 then
-            output = output .. input:sub(i, i)
-        else
-            break
-        end
-        i = i + 1
-    end
-
+  local output = ""
+  input = tostring(input)
+  if type(input) ~= "string" then
     return output
+  end
+  local i = 1
+  while (size > 0) do
+    local byte = input:byte(i)
+    if not byte then
+      return output
+    end
+    if byte < 128 then
+      -- ASCII byte
+      output = output .. input:sub(i, i)
+      size = size - 1
+    elseif byte < 192 then
+      -- Continuation bytes
+      output = output .. input:sub(i, i)
+    elseif byte < 244 then
+      -- Start bytes
+      output = output .. input:sub(i, i)
+      size = size - 1
+    end
+    i = i + 1
+  end
+
+  -- Add any bytes that are part of the sequence
+  while (true) do
+    local byte = input:byte(i)
+    if byte and byte >= 128 and byte < 192 then
+      output = output .. input:sub(i, i)
+    else
+      break
+    end
+    i = i + 1
+  end
+
+  return output
 end
 
 ns.util.CreateCategoryFrame = function(title, name, upper)
-    if upper == nil then
-        upper = UIParent
-    end
-    local relativePoint, offsetX, offsetY
-    if upper ~= UIParent then
-        relativePoint = "BOTTOMLEFT"
-        offsetX = 0
-        offsetY = -16
-    else
-        relativePoint = "TOPLEFT"
-        offsetX = 3
-        offsetY = -3
-    end
+  if upper == nil then
+    upper = UIParent
+  end
+  local relativePoint, offsetX, offsetY
+  if upper ~= UIParent then
+    relativePoint = "BOTTOMLEFT"
+    offsetX = 0
+    offsetY = -16
+  else
+    relativePoint = "TOPLEFT"
+    offsetX = 3
+    offsetY = -3
+  end
 
-    local f = CreateFrame("Frame", name, UIParent)
-    f:SetPoint("TOPLEFT", upper, relativePoint, offsetX, offsetY)
-    f:SetSize(176, 0)
-    f:SetFrameStrata("BACKGROUND")
+  local f = CreateFrame("Frame", name, UIParent)
+  f:SetPoint("TOPLEFT", upper, relativePoint, offsetX, offsetY)
+  f:SetSize(176, 0)
+  f:SetFrameStrata("BACKGROUND")
 
-    local titleFrame = CreateFrame("Frame", nil, f)
-    titleFrame:SetPoint("TOPLEFT", f, "TOPLEFT", 0, 0)
-    titleFrame.text = titleFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    titleFrame.text:SetPoint("LEFT")
-    titleFrame.text:SetFontHeight(12)
-    titleFrame.text:SetText(title)
-    titleFrame:SetSize(math.ceil(titleFrame.text:GetWidth()), 14)
+  local titleFrame = CreateFrame("Frame", nil, f)
+  titleFrame:SetPoint("TOPLEFT", f, "TOPLEFT", 0, 0)
+  titleFrame.text = titleFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  titleFrame.text:SetPoint("LEFT")
+  titleFrame.text:SetFontHeight(12)
+  titleFrame.text:SetText(title)
+  titleFrame:SetSize(math.ceil(titleFrame.text:GetWidth()), 14)
 
-    return f
+  return f
 end
 
 ns.util.ExpandFrame = function(f)
-    local children = { f:GetChildren() }
+  local children = { f:GetChildren() }
 
-    local shown = 0
-    for _, child in ipairs(children) do
-        if child:IsShown() then
-            shown = shown + 1
-        end
+  local shown = 0
+  for _, child in ipairs(children) do
+    if child:IsShown() then
+      shown = shown + 1
     end
+  end
 
-    f:SetHeight((14 * shown) + (shown - 1))
+  f:SetHeight((14 * shown) + (shown - 1))
 end
 
 ns.util.ProgressBar = function(parent, startColor, endColor)
-    if endColor == nil then
-        endColor = startColor
-    end
+  if endColor == nil then
+    endColor = startColor
+  end
 
-    local f = CreateFrame("Frame", nil, parent)
-    f:SetPoint("TOPLEFT", parent, "TOPLEFT", 0, -15)
-    f:SetSize(176, 14)
+  local f = CreateFrame("Frame", nil, parent)
+  f:SetPoint("TOPLEFT", parent, "TOPLEFT", 0, -15)
+  f:SetSize(176, 14)
 
-    f.label = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    f.label:SetPoint("LEFT")
-    f.label:SetPointsOffset(3, 0)
-    f.label:SetFontHeight(12)
-    f.label:SetTextColor(1, 1, 1)
+  f.label = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  f.label:SetPoint("LEFT")
+  f.label:SetPointsOffset(3, 0)
+  f.label:SetFontHeight(12)
+  f.label:SetTextColor(1, 1, 1)
 
-    f.value = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    f.value:SetPoint("RIGHT")
-    f.value:SetPointsOffset(-3, 0)
-    f.value:SetFontHeight(12)
-    f.value:SetTextColor(1, 1, 1)
+  f.value = f:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  f.value:SetPoint("RIGHT")
+  f.value:SetPointsOffset(-3, 0)
+  f.value:SetFontHeight(12)
+  f.value:SetTextColor(1, 1, 1)
 
-    local background = f:CreateTexture(nil, "BACKGROUND")
-    background:SetAllPoints()
-    background:SetSize(176, 14)
-    background:SetColorTexture(0, 0, 0, 0.12)
+  local background = f:CreateTexture(nil, "BACKGROUND")
+  background:SetAllPoints()
+  background:SetSize(176, 14)
+  background:SetColorTexture(0, 0, 0, 0.12)
 
-    f.bar = f:CreateTexture(nil, "ARTWORK")
-    f.bar:SetPoint("LEFT")
-    f.bar:SetSize(176, 14)
-    f.bar:SetTexture(LibSharedMedia:Fetch("statusbar", "Skyline"))
-    f.bar:SetGradient("HORIZONTAL", startColor, endColor)
+  f.bar = f:CreateTexture(nil, "ARTWORK")
+  f.bar:SetPoint("LEFT")
+  f.bar:SetSize(176, 14)
+  f.bar:SetTexture(LibSharedMedia:Fetch("statusbar", "Skyline"))
+  f.bar:SetGradient("HORIZONTAL", startColor, endColor)
 
-    f:Hide()
+  f:Hide()
 
-    return f
+  return f
 end
 
 ns.util.ParseKeystoneItemLink = function(itemLink)
-    local challengeMapID, level
+  local challengeMapID, level
 
-    if itemLink == nil then
-        return
+  if itemLink == nil then
+    return
+  end
+
+  local itemLinkSplit = strsplittable("|", itemLink)
+  local payload = strsplittable(":", itemLinkSplit[3])
+  if payload[2] ~= data.MythicPlusKeystoneItemID then
+    return
+  end
+
+  local skip = 0
+  if payload[14] ~= "" then
+    skip = tonumber(payload[14])
+  end
+
+  local numModifiers = payload[15 + skip]
+  for i = (15 + skip + 1), (15 + skip + (2 * numModifiers) - 1), 2 do
+    if payload[i] == "17" then
+      challengeMapID = tonumber(payload[i + 1])
+    elseif payload[i] == "18" then
+      level = tonumber(payload[i + 1])
     end
+  end
 
-    local itemLinkSplit = strsplittable("|", itemLink)
-    local payload = strsplittable(":", itemLinkSplit[3])
-    if payload[2] ~= data.MythicPlusKeystoneItemID then
-        return
-    end
-
-    local skip = 0
-    if payload[14] ~= "" then
-        skip = tonumber(payload[14])
-    end
-
-    local numModifiers = payload[15 + skip]
-    for i = (15 + skip + 1), (15 + skip + (2 * numModifiers) - 1), 2 do
-        if payload[i] == "17" then
-            challengeMapID = tonumber(payload[i + 1])
-        elseif payload[i] == "18" then
-            level = tonumber(payload[i + 1])
-        end
-    end
-
-    return challengeMapID, level
+  return challengeMapID, level
 end
+
+-- luacheck: push no max line length
+-- Borrowed from the official Blizzard code
+-- WARN: There are no mentions about the license.
+-- https://github.com/Gethe/wow-ui-source/blob/live/Interface/AddOns/Blizzard_Settings_Shared/Blizzard_SettingControls.lua
+ns.util.CreateSettingsCheckboxSliderInitializer = function(cbSetting, cbLabel, cbTooltip, sliderSetting, sliderOptions, sliderLabel, sliderTooltip, newTagID)
+  local initializerData =
+    {
+      name = cbLabel,
+      tooltip = cbTooltip,
+      cbSetting = cbSetting,
+      cbLabel = cbLabel,
+      cbTooltip = cbTooltip,
+      sliderSetting = sliderSetting,
+      sliderOptions = sliderOptions,
+      sliderLabel = sliderLabel,
+      sliderTooltip = sliderTooltip,
+      newTagID = newTagID,
+    };
+  local initializer = Settings.CreateSettingInitializer("SettingsCheckboxSliderControlTemplate", initializerData);
+  initializer:AddSearchTags(cbLabel, sliderLabel);
+  return initializer;
+end
+
+ns.util.CreateSettingsListSectionHeaderInitializer = function(name, tooltip, newTagID)
+  local initializerData = {name = name, tooltip = tooltip, newTagID = newTagID};
+  return Settings.CreateElementInitializer("SettingsListSectionHeaderTemplate", initializerData);
+end
+-- luacheck: pop
